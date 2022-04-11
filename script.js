67f6c48dc4886e90297be9e8eb6d2b08df155a0a
Conor Dunne <conordunne@outlook.ie>
2022-04-11 21:57:40 +0900

    Add extra div

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 const container = document.querySelector('#container');
 const content = document.createElement('div');
-const p = document.createElement('p');
+const p1 = document.createElement('p');
 const h3 = document.createElement('h3');
 const div2 = document.createElement('div');
+const p2 = document.createElement('p');
+const h1 = document.createElement('h1');
 
 content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 container.appendChild(content);
 
-p.style.color = "red";
-p.textContent = "Hey I’m red!";
-container.appendChild(p);
+p1.style.color = "red";
+p1.textContent = "Hey I’m red!";
+container.appendChild(p1);
 
 h3.style.color = "blue";
 h3.textContent = "I’m a blue h3!";
 container.appendChild(h3);
 
-div2.style.cssText = "background-color: pink; border: 5px solid black"
+div2.style.cssText = "background-color: pink; border: 5px solid black";
+h1.textContent = "I'm in a div";
+p2.textContent = "ME TOO!";
+div2.appendChild(h1);
+div2.appendChild(p2);
+container.appendChild(div2);
+
+
 
 
